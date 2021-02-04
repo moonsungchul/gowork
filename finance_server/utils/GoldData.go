f3140ea0c9414c56779c70289c0f7124b9e5b294
moonsungchul <moonsuchstar@firemstar.com>
2021-02-04 23:35:08 +0900

    update

--- finance_server/utils/GoldData.go
+++ finance_server/utils/GoldData.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -11,13 +12,28 @@ import (
 
 func main() {
 
-	mean := func(s series.Series) series.Series {
+	HighClose := func(s series.Series) series.Series {
 		floats := s.Float()
-		fmt.Println(floats)
-		//for _, f := range floats {
-		//	fmt.Println(f)
-		//}
-		return series.Floats(floats)
+		close := floats[4]
+		high := floats[2]
+		ch := close / high
+		return series.Floats(ch)
+	}
+
+	OpenClose := func(s series.Series) series.Series {
+		floats := s.Float()
+		open := floats[1]
+		close := floats[4]
+		ch := open / close
+		return series.Floats(ch)
+	}
+
+	LowClose := func(s series.Series) series.Series {
+		floats := s.Float()
+		low := floats[3]
+		close := floats[4]
+		ch := low / close
+		return series.Floats(ch)
 	}
 
 	csvfile, err := os.Open("C:/work/data_workspace/finance/src/gold.csv")
@@ -36,7 +52,29 @@ func main() {
 	//fmt.Println(close)
 	//close := df.Col("Close")
 	//fmt.Println(close)
-	//df.Capply(mean)
-	df.Rapply(mean)
+	//fmt.Println(df.Capply(mean))
+	//fmt.Println(df.Rapply(close_high))
+	closehigh := df.Rapply(HighClose)
+	openclose := df.Rapply(OpenClose)
+	lowclose := df.Rapply(LowClose)
+
+	closehigh.SetNames("CloseHigh")
+	openclose.SetNames("OpenClose")
+	lowclose.SetNames("LowClose")
+	df1 := df.CBind(closehigh)
+	df2 := df1.CBind(openclose)
+	df3 := df2.CBind(lowclose)
+	fmt.Println(df3)
+	fmt.Println(df3.Names())
+
+	file, err := os.OpenFile("final.csv", os.O_CREATE, os.FileMode(0644))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	df3.WriteCSV(w)
 
 }
